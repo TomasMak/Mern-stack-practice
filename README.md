1e42355b23e630f217c9c75a5966a3adda7ec0ac
TomasMak <t.makauskas@gmail.com>
2018-12-07 22:00:05 +0100

    Cleaned up and README added.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Full stack Shopping list practice with Mern stack.
+
+# React, Redux, Reactstrap used for the front, Express and MongoDB used for the back end.
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,8 +1,7 @@
-import uuid from 'uuid';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types'
 
 const initialState = {
-    items: [] ,
+    items: [],
     loading: false
 }
 
@@ -14,17 +13,17 @@ export default function (state = initialState, action) {
                 items: action.payload,
                 loading: false
             }
-            case DELETE_ITEM:
+        case DELETE_ITEM:
             return {
                 ...state,
                 items: state.items.filter(item => item._id !== action.payload)
             }
-            case ADD_ITEM:
+        case ADD_ITEM:
             return {
                 ...state,
                 items: [action.payload, ...state.items]
             }
-            case ITEMS_LOADING:
+        case ITEMS_LOADING:
             return {
                 ...state,
                 loading: true
